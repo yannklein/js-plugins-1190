030085ad1eb70d59e409b56fccf8be9cb991b156
Yann Klein <yann.klein@me.com>
2023-02-08 10:51:41 +0900

    lecture done

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 // //////////////////////
 // Rehearsal
 // //////////////////////
-// // 1. Select elements (buttoon)
-// const buttoon = document.querySelector("#click-me");
 
-// // 2. Listen to a click on the buttoon
-// buttoon.addEventListener("click", (event) => {
+// 1. Select an element (button)
+// const button = document.querySelector("#click-me");
+
+// // 2. Listen to an event (click on button)
+// button.addEventListener("click", (event) => {
+//   // 3. Change the DOM (change the text of the button and disable it)
 //   console.log(event);
-//   // 3. Change the DOM (change the inner text to  Loading.., make disabled)
 //   event.currentTarget.innerText = "Loading...";
 //   event.currentTarget.disabled = true;
 // });
@@ -16,44 +17,42 @@
 // //////////////////////
 // HTTP GET request
 // //////////////////////
-// 1. Select elements (input, search, list)
+
+// 1. Select elements (input, button, list)
 const input = document.querySelector("#keyword");
-const search = document.querySelector("#submit");
+const button = document.querySelector("#submit");
 const list = document.querySelector("#results");
 
-console.log("before the event listener");
-
-// 2. Listen to a click on search btn
-search.addEventListener("click", (event) => {
+console.log("before event");
+// 2. Listen to event (click on button)
+button.addEventListener("click", (event) => {
   console.log(event);
-  console.log("button clicked")
-  // Preventing the page from refreshing
+  console.log("clicked");
   event.preventDefault();
-
-  // 2.5 Fetch OMDB API (get the movies!)
-  const keyword = input.value;
-  const url = `https://www.omdbapi.com/?s=${keyword}&apikey=adf1f2d7`;
+  // 2.5 Fetch the OMDB API (get movies!)
+  const url = `https://www.omdbapi.com/?s=${input.value}&apikey=48727053`;
+  // const movies = fetch(url) NOT WORKING
   fetch(url)
     .then(response => response.json())
     .then((data) => {
+      console.log("data arrives");
       console.log(data);
-      console.log("data received")
       const movies = data.Search;
+      // 3. Change the DOM (display movies in the list)
       list.innerHTML = "";
       movies.forEach((movie) => {
-        // 3. Change the DOM (display the movies in the list)
-        list.insertAdjacentHTML(
-          "beforeend",
-          `<li class='list-inline-item'>
-            <img src="${movie.Poster}" alt="" />
-            <p>${movie.Title}</p>
-          </li>`);
+        list.insertAdjacentHTML("beforeend", `
+        <li class='list-inline-item'>
+          <img src="${movie.Poster}" alt="" />
+          <p>${movie.Title}</p>
+        </li>
+        `);
       });
-    })
-  console.log("after the fetch code");
+      console.log("after change the dom");
+    });
+  console.log("after the fetch");
 });
-
-
+console.log("after the addEventListener");
 
 // //////////////////////
 // HTTP POST request
